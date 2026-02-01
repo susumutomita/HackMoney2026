e08963b14a6b84b634345c73acabd5132e8cca87
exe.dev user <exedev@zerokey.exe.xyz>
2026-02-01 11:39:28 +0000

    feat(frontend): add payment status modal (processing/success/fail)

--- packages/frontend/src/components/index.ts
+++ packages/frontend/src/components/index.ts
@@ -1,5 +1,6 @@
 export { TransactionAnalyzer } from "./TransactionAnalyzer";
 export { AnalysisHistory } from "./AnalysisHistory";
 export { PayConfirmModal } from "./PayConfirmModal";
+export { PaymentStatusModal } from "./PaymentStatusModal";
 export { PolicyList } from "./PolicyList";
 export { StatsCards } from "./StatsCards";
